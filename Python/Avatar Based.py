07550b3d70a705ea28f85882307e3b7bacbc27b5
Marcus Vinicius de Araújo <89282421+Markeid@users.noreply.github.com>
2022-05-06 14:35:55 -0300

    Add files via upload

---
+++ Python/Avatar Based.py
@@ -0,0 +1,9 @@
+!pip install pagan
+import pagan
+inpt = 'Python Developer'
+img = pagan.Avatar(inpt, pagan.SHA512)
+img.show()
+outpath = 'output/'
+filename = inpt
+img.save(outpath, filename)
+img.change('new input', pagan.SHA256)
